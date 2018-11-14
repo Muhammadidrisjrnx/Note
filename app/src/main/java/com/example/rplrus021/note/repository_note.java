b033b05c7e54f274e9efdef98d32e7d816970260
idrisjrnxgbox <idris.xrplsmkrus@gmail.com>
2018-11-14 16:50:04 +0700

    .....

--- app/src/main/java/com/example/rplrus021/note/repository_note.java
+++ app/src/main/java/com/example/rplrus021/note/repository_note.java
@@ -2,6 +2,8 @@ package com.example.rplrus021.note;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
+import android.support.v4.content.AsyncTaskLoader;
+import android.os.AsyncTask;
 
 import java.util.List;
 
@@ -16,14 +18,37 @@ public class repository_note {
     }
 
     public void insert(model_data_note model_data_note) {
-
+        new insert_asnyctask(noteDao).execute(model_data_note);
     }
 
     public void delete(model_data_note model_data_note) {
-
+        new delete_asnyctask(noteDao).execute(model_data_note);
     }
 
-    public class insert_asnyctask extends AsyncTask<model_data_note, Void, Void> {
+    private class insert_asnyctask extends AsyncTask<model_data_note, Void, Void> {
+        private NoteDao noteDao;
 
+        private insert_asnyctask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(model_data_note... model_data_notes) {
+            noteDao.insert(model_data_notes[0]);
+            return null;
+        }
+    }
+    private class delete_asnyctask extends AsyncTask<model_data_note, Void, Void> {
+        private NoteDao noteDao;
+
+        private delete_asnyctask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(model_data_note... model_data_notes) {
+            noteDao.delete(model_data_notes[0]);
+            return null;
+        }
     }
 }
